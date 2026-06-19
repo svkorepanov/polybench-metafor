979caa54aea0620f6cf24bb61dafd1cf01a217b0
Sergey Korepanov <skorepanov@walletteam.org>
2026-06-19 09:44:52 +0100

    revert formating

--- linear-algebra/kernels/2mm/2mm.omp-tile.F90
+++ linear-algebra/kernels/2mm/2mm.omp-tile.F90
@@ -1,160 +1,159 @@
 !******************************************************************************
 !
 !  2mm.F90: This file is part of the PolyBench/Fortran 1.0 test suite.
-!
+! 
 !  Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
 !  Web address: http://polybench.sourceforge.net
 !
 !******************************************************************************
 
-! Include polybench common header.
+! Include polybench common header. 
 #include <fpolybench.h>
 
-! Include benchmark-specific header.
-! Default data type is double, default size is 4000.
+! Include benchmark-specific header. 
+! Default data type is double, default size is 4000. 
 #include "2mm.h"
 
-program two_mm
-   implicit none
-
-   polybench_2d_array_decl(tmp, data_type, nj, ni)
-   polybench_2d_array_decl(a, data_type, nk, ni)
-   polybench_2d_array_decl(b, data_type, nj, nk)
-   polybench_2d_array_decl(c, data_type, nl, nj)
-   polybench_2d_array_decl(d, data_type, nl, ni)
-   data_type :: alpha, beta
-   polybench_declare_prevent_dce_vars
-   polybench_declare_instruments
-
-   !     Allocation of Arrays
-   polybench_alloc_2d_array(tmp, nj, ni)
-   polybench_alloc_2d_array(a, nk, ni)
-   polybench_alloc_2d_array(b, nj, nk)
-   polybench_alloc_2d_array(c, nl, nj)
-   polybench_alloc_2d_array(d, nl, ni)
-
-   !     Initialization
-   call init_array(alpha, beta, a, b, c, d, ni, nj, &
-   nk, nl)
-
-   !     Kernel Execution
-   polybench_start_instruments
-
-   call kernel_2mm(alpha, beta, tmp, a, b, c, d, &
-   ni, nj, nk, nl)
-
-
-   polybench_stop_instruments
-   polybench_print_instruments
-
-   !     Prevent dead-code elimination. All live-out data must be printed
-   !     by the function call in argument.
-   polybench_prevent_dce(print_array(d, ni, nl)) ;
-
-   !     Deallocation of Arrays
-   polybench_dealloc_array(tmp)
-   polybench_dealloc_array(a)
-   polybench_dealloc_array(b)
-   polybench_dealloc_array(c)
-   polybench_dealloc_array(d)
-
-   contains
-
-   subroutine init_array(alpha, beta, a, b, c, d, ni, nj, &
-   nk, nl)
+      program two_mm
       implicit none
 
-      data_type, dimension(nk, ni) :: a
-      data_type, dimension(nj, nk) :: b
-      data_type, dimension(nl, nj) :: c
-      data_type, dimension(nl, ni) :: d
-      data_type :: alpha, beta
-      integer :: ni, nj, nk, nl
-      integer :: i, j
+      POLYBENCH_2D_ARRAY_DECL(tmp ,DATA_TYPE, NJ, NI)
+      POLYBENCH_2D_ARRAY_DECL(a ,DATA_TYPE, NK, NI) 
+      POLYBENCH_2D_ARRAY_DECL(b ,DATA_TYPE, NJ, NK) 
+      POLYBENCH_2D_ARRAY_DECL(c ,DATA_TYPE, NL, NJ) 
+      POLYBENCH_2D_ARRAY_DECL(d ,DATA_TYPE, NL, NI) 
+      DATA_TYPE :: alpha, beta
+      polybench_declare_prevent_dce_vars
+      polybench_declare_instruments
 
-      alpha = 32412;
-      beta = 2123;
+!     Allocation of Arrays
+      POLYBENCH_ALLOC_2D_ARRAY(tmp, NJ, NI)
+      POLYBENCH_ALLOC_2D_ARRAY(a, NK, NI)
+      POLYBENCH_ALLOC_2D_ARRAY(b, NJ, NK)
+      POLYBENCH_ALLOC_2D_ARRAY(c, NL, NJ)
+      POLYBENCH_ALLOC_2D_ARRAY(d, NL, NI)
 
-      do i = 1, ni
-         do j = 1, nk
-            a(j, i) = dble((i - 1) * (j - 1)) / ni
-         end do
-      end do
+!     Initialization
+      call init_array(alpha, beta, a, b, c, d, NI, NJ,  &
+                                             NK, NL)
 
-      do i = 1, nk
-         do j = 1, nj
-            b(j, i) = (dble((i - 1) * (j)))/ nj
-         end do
-      end do
+!     Kernel Execution
+      polybench_start_instruments
 
-      do i = 1, nl
-         do j = 1, nj
-            c(j, i) = (dble(i - 1) * (j + 2))/ nl
-         end do
-      end do
-
-      do i = 1, ni
-         do j = 1, nl
-            d(j, i) = (dble(i - 1) * (j + 1))/ nk
-         end do
-      end do
-   end subroutine
+      call kernel_2mm(alpha, beta, tmp, a, b, c, d,  &
+                                  NI, NJ, NK, NL)
 
 
-   subroutine print_array(d, ni, nl)
-      implicit none
+      polybench_stop_instruments
+      polybench_print_instruments
 
-      data_type, dimension(nl, ni) :: d
-      integer :: nl, ni
-      integer :: i, j
-      do i = 1, ni
-         do j = 1, nl
-            write(0, data_printf_modifier) d(j, i)
+!     Prevent dead-code elimination. All live-out data must be printed
+!     by the function call in argument. 
+      polybench_prevent_dce(print_array(d, NI, NL));
+
+!     Deallocation of Arrays 
+      POLYBENCH_DEALLOC_ARRAY(tmp)
+      POLYBENCH_DEALLOC_ARRAY(a)
+      POLYBENCH_DEALLOC_ARRAY(b)
+      POLYBENCH_DEALLOC_ARRAY(c)
+      POLYBENCH_DEALLOC_ARRAY(d)
+
+      contains
+
+        subroutine init_array(alpha, beta, a, b, c ,d, ni, nj,  &
+             nk, nl)
+        implicit none
+
+        DATA_TYPE, dimension(nk, ni) :: a
+        DATA_TYPE, dimension(nj, nk) :: b
+        DATA_TYPE, dimension(nl, nj) :: c
+        DATA_TYPE, dimension(nl, ni) :: d
+        DATA_TYPE :: alpha, beta
+        integer :: ni, nj, nk, nl
+        integer :: i, j
+
+        alpha = 32412;
+        beta = 2123; 
+
+        do i = 1, ni
+          do j = 1, nk
+            a(j,i) = DBLE((i-1) * (j-1)) / ni
+          end do
+        end do
+
+        do i = 1, nk
+          do j = 1, nj
+            b(j,i) = (DBLE((i-1) * (j)))/ nj
+          end do
+        end do
+
+        do i = 1, nl
+          do j = 1, nj
+            c(j,i) = (DBLE(i-1) * (j+2))/ nl
+          end do
+        end do
+
+        do i = 1, ni
+          do j = 1, nl
+            d(j,i) = (DBLE(i-1) * (j+1))/ nk
+          end do
+        end do
+        end subroutine
+
+
+        subroutine print_array(d, ni, nl)
+        implicit none
+
+        DATA_TYPE, dimension(nl, ni) :: d
+        integer :: nl, ni
+        integer :: i, j
+        do i = 1, ni
+          do j = 1, nl
+            write(0, DATA_PRINTF_MODIFIER) d(j,i) 
 
             if (mod(((i - 1) * ni) + j - 1, 20) == 0) then
-               write(0, *)
+              write(0, *)
             end if
 
-         end do
-      end do
-      write(0, *)
-   end subroutine
+          end do
+        end do
+        write(0, *)
+        end subroutine
 
 
-   subroutine kernel_2mm(alpha, beta, tmp, a, b, c, d, &
-   ni, nj, nk, nl)
-      implicit none
+        subroutine kernel_2mm(alpha, beta, tmp, a, b, c, d,  &
+                                              ni, nj, nk, nl)
+        implicit none
 
-      data_type, dimension(nj, ni) :: tmp
-      data_type, dimension(nk, ni) :: a
-      data_type, dimension(nj, nk) :: b
-      data_type, dimension(nl, nj) :: c
-      data_type, dimension(nl, ni) :: d
-      data_type :: alpha, beta
-      integer :: ni, nj, nk, nl
-      integer :: i, j, k
+        DATA_TYPE, dimension(nj, ni) :: tmp
+        DATA_TYPE, dimension(nk, ni) :: a
+        DATA_TYPE, dimension(nj, nk) :: b
+        DATA_TYPE, dimension(nl, nj) :: c
+        DATA_TYPE, dimension(nl, ni) :: d
+        DATA_TYPE :: alpha, beta
+        integer :: ni, nj, nk, nl
+        integer :: i, j, k
 
-      !$pragma scop
-      !$omp tile sizes(32,32)
-      do i = 1, _pb_ni
-         do j = 1, _pb_nj
-            tmp(j, i) = 0.0
-            do k = 1, _pb_nk
-               tmp(j, i) = tmp(j, i) + alpha * a(k, i) * b(j, k)
+!$pragma scop
+        !$omp tile sizes(32,32)
+        do i = 1, _PB_NI
+          do j = 1, _PB_NJ
+            tmp(j,i) = 0.0
+            do k = 1, _PB_NK
+              tmp(j,i) = tmp(j,i) + alpha * a(k,i) * b(j,k)
             end do
-         end do
-      end do
-      
-      !$omp tile sizes(32,32)
-      do i = 1, _pb_ni
-         do j = 1, _pb_nl
-            d(j, i) = d(j, i) * beta
-            do k = 1, _pb_nj
-               d(j, i) = d(j, i) + tmp(k, i) * c(j, k)
+          end do
+        end do
+        !$omp tile sizes(32,32)
+        do i = 1, _PB_NI
+          do j = 1, _PB_NL
+            d(j,i) = d(j,i) * beta
+            do k = 1, _PB_NJ
+              d(j,i) = d(j,i) + tmp(k,i) * c(j,k)
             end do
-         end do
-      end do
-      !$pragma endscop
-   end subroutine
+          end do
+        end do
+!$pragma endscop
+        end subroutine 
 
-end program
+      end program
--- stencils/adi/adi.omp-tile.F90
+++ stencils/adi/adi.omp-tile.F90
@@ -1,154 +1,154 @@
 !*****************************************************************************
-!
-!  adi.F90: This file is part of the PolyBench/Fortran 1.0 test suite.
-!
-!  Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
-!  Web address: http://polybench.sourceforge.net
-!
+!                                                                             
+!  adi.F90: This file is part of the PolyBench/Fortran 1.0 test suite.          
+!                                                                             
+!  Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>                   
+!  Web address: http://polybench.sourceforge.net                              
+!                                                                             
 !*****************************************************************************
 
-! Include polybench common header.
+! Include polybench common header. 
 #include <fpolybench.h>
 
-! Include benchmark-specific header.
-! Default data type is double, default size is 10x1024x1024.
+! Include benchmark-specific header. 
+! Default data type is double, default size is 10x1024x1024. 
 #include "adi.h"
 
-program adi
-   implicit none
-
-   polybench_2d_array_decl(x, data_type, n, n)
-   polybench_2d_array_decl(a, data_type, n, n)
-   polybench_2d_array_decl(b, data_type, n, n)
-   polybench_declare_prevent_dce_vars
-   polybench_declare_instruments
-
-   !     Allocation of Arrays
-   polybench_alloc_2d_array(x, n, n)
-   polybench_alloc_2d_array(a, n, n)
-   polybench_alloc_2d_array(b, n, n)
-
-   !     Initialization
-   call init_array(n, x, a, b)
-
-   !     Kernel Execution
-   polybench_start_instruments
-
-   call kernel_adi(tsteps, n, x, a, b)
-
-   polybench_stop_instruments
-   polybench_print_instruments
-
-   !     Prevent dead-code elimination. All live-out data must be printed
-   !     by the function call in argument.
-   polybench_prevent_dce(print_array(n, x)) ;
-
-   !     Deallocation of Arrays
-   polybench_dealloc_array(x)
-   polybench_dealloc_array(a)
-   polybench_dealloc_array(b)
-
-   contains
-
-   subroutine init_array(n, x, a, b)
+      program adi
       implicit none
 
-      data_type, dimension(n, n) :: a
-      data_type, dimension(n, n) :: x
-      data_type, dimension(n, n) :: b
-      integer :: n
-      integer :: i, j
+      POLYBENCH_2D_ARRAY_DECL(x,DATA_TYPE,N, N)
+      POLYBENCH_2D_ARRAY_DECL(a,DATA_TYPE,N, N)
+      POLYBENCH_2D_ARRAY_DECL(b,DATA_TYPE,N, N)
+      polybench_declare_prevent_dce_vars
+      polybench_declare_instruments
 
-      do i = 1, n
-         do j = 1, n
-            x(j, i) = (dble((i - 1) * (j)) + 1.0d0) / dble(n)
-            a(j, i) = (dble((i - 1) * (j + 1)) + 2.0d0) / dble(n)
-            b(j, i) = (dble((i - 1) * (j + 2)) + 3.0d0) / dble(n)
-         end do
-      end do
-   end subroutine
+!     Allocation of Arrays
+      POLYBENCH_ALLOC_2D_ARRAY(x, N, N)
+      POLYBENCH_ALLOC_2D_ARRAY(a, N, N)
+      POLYBENCH_ALLOC_2D_ARRAY(b, N, N)
+
+!     Initialization
+      call init_array(N, x, a, b)
+
+!     Kernel Execution
+      polybench_start_instruments
+
+      call kernel_adi(TSTEPS, N, x, a, b)
+
+      polybench_stop_instruments
+      polybench_print_instruments
+
+!     Prevent dead-code elimination. All live-out data must be printed
+!     by the function call in argument. 
+      polybench_prevent_dce(print_array(N, x));
+
+!     Deallocation of Arrays 
+      POLYBENCH_DEALLOC_ARRAY(x)
+      POLYBENCH_DEALLOC_ARRAY(a)
+      POLYBENCH_DEALLOC_ARRAY(b)
+
+      contains
+
+        subroutine init_array(n, x, a, b)
+        implicit none
+
+        DATA_TYPE, dimension(n, n) :: a
+        DATA_TYPE, dimension(n, n) :: x
+        DATA_TYPE, dimension(n, n) :: b
+        integer :: n
+        integer :: i, j
+
+        do i = 1, n
+          do j = 1, n
+            x(j, i) = (DBLE((i - 1) * (j)) + 1.0D0) / DBLE(n)
+            a(j, i) = (DBLE((i - 1) * (j + 1)) + 2.0D0) / DBLE(n)
+            b(j, i) = (DBLE((i - 1) * (j + 2)) + 3.0D0) / DBLE(n)
+          end do
+        end do
+        end subroutine
 
 
-   subroutine print_array(n, x)
-      implicit none
+        subroutine print_array(n, x)
+        implicit none
 
-      data_type, dimension(n, n) :: x
-      integer :: n
-      integer :: i, j
+        DATA_TYPE, dimension(n, n) :: x
+        integer :: n
+        integer :: i, j
 
-      do i = 1, n
-         do j = 1, n
-            write(0, data_printf_modifier) x(j, i)
+        do i = 1, n
+          do j = 1, n
+            write(0, DATA_PRINTF_MODIFIER) x(j, i)
             if (mod(((i - 1) * n) + j - 1, 20) == 0) then
-               write(0, *)
+              write(0, *)
             end if
-         end do
-      end do
-      write(0, *)
-   end subroutine
+          end do
+        end do
+        write(0, *)
+        end subroutine
 
 
-   subroutine kernel_adi(tsteps, n, x, a, b)
-      implicit none
+        subroutine kernel_adi(tsteps, n, x, a, b)
+        implicit none
 
-      data_type, dimension(n, n) :: a
-      data_type, dimension(n, n) :: x
-      data_type, dimension(n, n) :: b
-      integer :: n, tsteps
-      integer :: i1, i2, t
+        DATA_TYPE, dimension(n, n) :: a
+        DATA_TYPE, dimension(n, n) :: x
+        DATA_TYPE, dimension(n, n) :: b
+        integer :: n, tsteps
+        integer :: i1, i2, t
 
-      !$pragma scop
-      do t = 1, _pb_tsteps
-        !$omp tile sizes(32,32)
-         do i1 = 1, _pb_n
-            do i2 = 2, _pb_n
-               x(i2, i1) = x(i2, i1) - ((x(i2 - 1, i1) * a(i2, i1)) / &
-               b(i2 - 1, i1))
-               b(i2, i1) = b(i2, i1) - ((a(i2, i1) * a(i2, i1)) / &
-               b(i2 - 1, i1))
+!$pragma scop
+        do t = 1, _PB_TSTEPS
+          !$omp tile sizes(32,32)
+          do i1 = 1, _PB_N
+            do i2 = 2, _PB_N
+              x(i2, i1) = x(i2, i1) - ((x(i2 - 1, i1) * a(i2, i1)) / &
+                          b(i2 - 1, i1))
+              b(i2, i1) = b(i2, i1) - ((a(i2, i1) * a(i2, i1)) / &
+                          b(i2 - 1, i1))
             end do
-         end do
+          end do
 
-         do i1 = 1, _pb_n
-            x(_pb_n, i1) = x(_pb_n, i1) / b(_pb_n, i1)
-         end do
+          do i1 = 1, _PB_N
+            x(_PB_N, i1) = x(_PB_N, i1) / b(_PB_N, i1)
+          end do
 
-        !$omp tile sizes(32,32)
-         do i1 = 1, _pb_n
-            do i2 = 1, _pb_n - 2
-               x(_pb_n - i2, i1) = (x(_pb_n - i2, i1) - &
-               (x(_pb_n - i2 - 1, i1) * &
-               a(_pb_n - i2 - 1, i1))) / &
-               b(_pb_n - i2 - 1, i1)
+          !$omp tile sizes(32,32)
+          do i1 = 1, _PB_N 
+            do i2 = 1, _PB_N - 2
+              x(_PB_N - i2, i1) = (x(_PB_N - i2, i1) - &
+                                    (x(_PB_N - i2 - 1, i1) * &
+                                    a(_PB_N - i2 - 1, i1))) / &
+                                    b(_PB_N - i2 - 1, i1)
             end do
-         end do
+          end do
 
-        !$omp tile sizes(32,32)
-         do i1 = 2, _pb_n
-            do i2 = 1, _pb_n
-               x(i2, i1) = x(i2, i1) - x(i2, i1 - 1) * a(i2, i1) / &
-               b(i2, i1 - 1)
-               b(i2, i1) = b(i2, i1) - a(i2, i1) * a(i2, i1) / &
-               b(i2, i1 - 1)
-
+          !$omp tile sizes(32,32)
+          do i1 = 2, _PB_N 
+            do i2 = 1, _PB_N 
+              x(i2, i1) = x(i2, i1) - x(i2, i1 - 1) * a(i2, i1) / &
+                          b(i2, i1 - 1)
+              b(i2, i1) = b(i2, i1) - a(i2, i1) * a(i2, i1) / &
+                          b(i2, i1 - 1)
+              
             end do
-         end do
+          end do
 
-         do i2 = 1, _pb_n
-            x(i2, _pb_n) = x(i2, _pb_n) / b(i2, _pb_n)
-         end do
+          do i2 = 1, _PB_N
+            x(i2, _PB_N) = x(i2, _PB_N) / b(i2, _PB_N)
+          end do
 
-        !$omp tile sizes(32,32)
-         do i1 = 1, _pb_n - 2
-            do i2 = 1, _pb_n
-               x(i2, _pb_n - i1) = (x(i2, _pb_n - i1) - &
-               x(i2, _pb_n - i1 - 1) * &
-               a(i2, _pb_n - i1 - 1)) / &
-               b(i2, _pb_n - i1)
+          !$omp tile sizes(32,32)
+          do i1 = 1, _PB_N - 2
+            do i2 = 1, _PB_N
+              x(i2, _PB_N - i1) = (x(i2, _PB_N - i1) - &
+                                    x(i2, _PB_N - i1 - 1) * &
+                                    a(i2, _PB_N - i1 - 1)) / &
+                                    b(i2, _PB_N - i1)
             end do
-         end do
-      end do
-      !$pragma endscop
-   end subroutine
+          end do
+        end do
+!$pragma endscop
+        end subroutine
 
-end program
+      end program
